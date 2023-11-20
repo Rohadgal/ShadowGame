dd998aafdd01a2f03aa5550fdd3ad158541c4bf3
Rohadgal <gal.frias@gmail.com>
2023-11-20 17:42:40 -0600

    Release interactable with key E

--- ShadowGamePlay/Assets/InteractionManager.cs
+++ ShadowGamePlay/Assets/InteractionManager.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 
 public class InteractionManager : MonoBehaviour {
     public float interactionRange = 3f;
+    [SerializeField]
     public LayerMask interactableLayer;
+    bool isObjectGrabbed;
+    Interactable interactable;
+    RaycastHit hit;
+
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             Debug.Log("E PRESSED");
-            TryInteract();
+            if(!isObjectGrabbed) {
+                TryInteract();
+                return;
+            }
+            isObjectGrabbed = false;
+            if(interactable != null) { }
+            interactable.Interact(hit);
         }
     }
 
     void TryInteract() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
         if (Physics.Raycast(ray, out hit, interactionRange, interactableLayer)) {
-            Interactable interactable = hit.collider.GetComponent<Interactable>();
+            interactable = hit.collider.GetComponent<Interactable>();
             if (interactable != null) {
                 interactable.Interact(hit);
+                isObjectGrabbed = true;
             }
         }
     }
