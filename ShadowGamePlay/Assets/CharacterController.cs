897e6f83b27314a0025e96380f126f77fef4ea63
lfeq <panchocosa@gmail.com>
2023-11-21 14:40:37 -0600

    Fixed Drop bug

--- ShadowGamePlay/Assets/CharacterController.cs
+++ ShadowGamePlay/Assets/CharacterController.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-    [SerializeField]
-    float movementSpeed = 1.0f;
-    [SerializeField]
-    float sensitivity = 1.0f;
 
-    void Update() {
+    [SerializeField]
+    private float movementSpeed = 1.0f;
+
+    [SerializeField]
+    private float sensitivity = 1.0f;
+
+    private void Update() {
         // Player Movement
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
-            float horizontal = Input.GetAxis("Horizontal");
-            float depth = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float depth = Input.GetAxis("Vertical");
+        if (horizontal != 0 || depth != 0) {
             Vector3 movement = new Vector3(horizontal, 0f, depth).normalized;
             transform.Translate(movement * movementSpeed * Time.deltaTime);
         }
--- ShadowGamePlay/Assets/InteractionManager.cs
+++ ShadowGamePlay/Assets/InteractionManager.cs
@@ -4,14 +4,18 @@ public class InteractionManager : MonoBehaviour {
     public float interactionRange = 3f;
     public LayerMask interactableLayer;
 
-    void Update() {
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             Debug.Log("E PRESSED");
             TryInteract();
         }
     }
 
-    void TryInteract() {
+    private void TryInteract() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, interactionRange, interactableLayer)) {
